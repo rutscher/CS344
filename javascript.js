e05dbaa45e1d206bcd2bbdad844b5830f40be930
rutscher <robrutscher@gmail.com>
2013-11-23 11:32:18 -0600

    Added FAQ and Updated Index some more

--- javascript.js
+++ javascript.js
@@ -1,6 +1,17 @@
 window.onload=function() {
 	 
     $('#test').rssfeed('http://sustainabilityhousewsu.wordpress.com/tag/website-General/feed/');
+
+    $('dd').hide();
+
+    $('dt').click(function(e){
+    // All dt elements after this dt element until the next dt element
+    // Will be hidden or shown depending on it's current visibility
+    
+    $(this).nextUntil('dt').slideToggle();
+});
+
+
 }
 
 
